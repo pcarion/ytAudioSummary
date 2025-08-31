c57e05ad9888ca5e81efcb5d143053119974670b
Pierre Carion <pcarion@gmail.com>
2025-08-31 16:10:51 -0700

    add manifest

--- packages/webExtension/wxt.config.ts
+++ packages/webExtension/wxt.config.ts
@@ -3,4 +3,16 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'ytAudioSummary',
+    description: 'YouTube Audio Summary',
+    icons: {
+      16: 'icon/16.png',
+      32: 'icon/32.png',
+      48: 'icon/48.png',
+      96: 'icon/96.png',
+      128: 'icon/128.png'
+    }
+  }
+
 });
